7d5ed2818c3b38f173403723de7cd78b0e4b8cbc
zhufengnodejs <zhufengnodejs@126.com>
2017-05-18 19:11:12 +0800

    左右箭头导航

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,21 +6,37 @@ export default class Slider extends React.Component{
         super(props);
         this.state = {pos:0};//默认索引
     }
-    componentDidMount(){//组件加载完成之后
+    //turn表示切换 n表示切换的步长
+    turn(n){
+        let pos = this.state.pos;//获取旧索引
+        pos+=n;
+        if(pos >= this.props.images.length){
+             pos= 0;
+        }
+        if(pos < 0){
+            pos=this.props.images.length-1;
+        }
+        this.setState({pos});
+    }
+    play(){//开启定时器进行自动轮播
         this.$timer = setInterval(()=>{
-            let pos = this.state.pos;
-            pos++;
-            this.setState({pos});
+            this.turn(1);
         },this.props.interval*1000)
+    }
+    componentDidMount(){//组件加载完成之后
+        if(this.props.autoplay){
+            this.play();
+        }
     }
     render(){
         let images = this.props.images;
         let style = {
             width:200*images.length,
-            left:this.state.pos * -200
+            left:this.state.pos * -200,
+            transitionDuration:this.props.speed+'s'
         }
         return (
-            <div className="wrapper">
+            <div onMouseOver={()=>clearInterval(this.$timer)} onMouseOut={()=>this.play()} className="wrapper">
                 <ul style={style} className="sliders">
                     {
                         images.map((image,index)=>(
@@ -30,6 +46,10 @@ export default class Slider extends React.Component{
                         ))
                     }
                 </ul>
+                <div className="arrows">
+                    <span className="arrow arrow-left" onClick={()=>this.turn(-1)}>&lt;</span>
+                    <span className="arrow arrow-right" onClick={()=>this.turn(1)}>&gt;</span>
+                </div>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -9,7 +9,12 @@ let images = [
 ]
 ReactDOM.render((
     <Slider
-      images={images}
-      interval={2}
+      images={images} //图片
+      interval={2}    //多长时间轮播一次
+      speed={1}       //播放的速度
+      autoplay={true} //是否启用自动轮播
+      pause={true}    //当鼠标移动上去自动自动暂停
+      arrows={true}   //是否有箭头导航
+      dots={true}     //是否有点状导航
     />
 ),document.querySelector('#root'));
